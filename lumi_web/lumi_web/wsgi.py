170bbd46e5c862abbee5835af40f1ba1c769a3f6
anuragtechzzz-dotcom <anuragtechzzz@gmail.com>
2026-03-18 07:47:12 +0530

    Update UI to Lumi neon dark theme and replace old images

--- lumi_web/lumi_web/wsgi.py
+++ lumi_web/lumi_web/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for psykh_web project.
+WSGI config for lumi_web project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'psykh_web.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lumi_web.settings')
 
 application = get_wsgi_application()
